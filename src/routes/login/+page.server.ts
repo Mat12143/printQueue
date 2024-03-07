4b1e11edd290d4878046262a265d0ebd454da1f7
Mat12143 <skymatti12@gmail.com>
2024-03-07 15:37:42 +0100

    Fixed wrong IP Address on fail log

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,43 +1,42 @@
 import { checkAdminPass, generateAdminCookie } from '$lib/db';
 import { authLogger } from '$lib/logging';
-import { fail, redirect } from '@sveltejs/kit';
+import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ locals }) => {
-    if (locals.admin) throw redirect(303, '/');
+	if (locals.admin) throw redirect(303, '/');
 };
 
 export const actions = {
-    login: async ({ request, cookies, getClientAddress }) => {
-        const formData = await request.formData();
+	login: async ({ request, cookies }) => {
+		const ipv4Add = request.headers.get('x-forwarded-host');
 
-        let ipAdd: string = getClientAddress();
-        let ipv4Add = ipAdd.split(':')[ipAdd.split(':').length - 1];
+		const formData = await request.formData();
 
-        const password = formData.get('password');
+		const password = formData.get('password');
 
-        if (!checkAdminPass(password)) {
-            authLogger.error(`${ipv4Add} failed to login with password "${password}"`);
-            return {
-                error: true,
-                message: 'Password errata'
-            };
-        }
+		if (!checkAdminPass(password)) {
+			authLogger.error(`${ipv4Add} failed to login with password "${password}"`);
+			return {
+				error: true,
+				message: 'Password errata'
+			};
+		}
 
-        const resp = generateAdminCookie();
-        if (resp.err || !resp.cookie)
-            return {
-                error: true,
-                message: 'Errore nel login'
-            };
+		const resp = generateAdminCookie();
+		if (resp.err || !resp.cookie)
+			return {
+				error: true,
+				message: 'Errore nel login'
+			};
 
-        cookies.set('session', resp.cookie, {
-            path: '/',
-            sameSite: 'strict',
-            httpOnly: true,
-            maxAge: 60 * 60 * 24,
-            secure: false
-        });
+		cookies.set('session', resp.cookie, {
+			path: '/',
+			sameSite: 'strict',
+			httpOnly: true,
+			maxAge: 60 * 60 * 24,
+			secure: false
+		});
 
-        throw redirect(303, '/');
-    }
+		throw redirect(303, '/');
+	}
 };
