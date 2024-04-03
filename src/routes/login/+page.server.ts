51688c66e29b428778181c8bd53420ccb4b95f96
Mat12143 <skymatti12@gmail.com>
2024-04-03 17:58:01 +0200

    fix: real ip on error logs

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -3,40 +3,40 @@ import { authLogger } from '$lib/logging';
 import { redirect } from '@sveltejs/kit';
 
 export const load = async ({ locals }) => {
-	if (locals.admin) throw redirect(303, '/');
+    if (locals.admin) throw redirect(303, '/');
 };
 
 export const actions = {
-	login: async ({ request, cookies }) => {
-		const ipv4Add = request.headers.get('x-forwarded-host');
+    login: async ({ request, cookies }) => {
+        const ipv4Add = request.headers.get('x-forwarded-for');
 
-		const formData = await request.formData();
+        const formData = await request.formData();
 
-		const password = formData.get('password');
+        const password = formData.get('password');
 
-		if (!checkAdminPass(password)) {
-			authLogger.error(`${ipv4Add} failed to login with password "${password}"`);
-			return {
-				error: true,
-				message: 'Password errata'
-			};
-		}
+        if (!checkAdminPass(password)) {
+            authLogger.error(`${ipv4Add} failed to login with password "${password}"`);
+            return {
+                error: true,
+                message: 'Password errata'
+            };
+        }
 
-		const resp = generateAdminCookie();
-		if (resp.err || !resp.cookie)
-			return {
-				error: true,
-				message: 'Errore nel login'
-			};
+        const resp = generateAdminCookie();
+        if (resp.err || !resp.cookie)
+            return {
+                error: true,
+                message: 'Errore nel login'
+            };
 
-		cookies.set('session', resp.cookie, {
-			path: '/',
-			sameSite: 'strict',
-			httpOnly: true,
-			maxAge: 60 * 60 * 24,
-			secure: false
-		});
+        cookies.set('session', resp.cookie, {
+            path: '/',
+            sameSite: 'strict',
+            httpOnly: true,
+            maxAge: 60 * 60 * 24,
+            secure: false
+        });
 
-		throw redirect(303, '/');
-	}
+        throw redirect(303, '/');
+    }
 };
